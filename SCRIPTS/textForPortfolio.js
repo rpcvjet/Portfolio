50ef50724be8de01096d46e8703007d5a0bf029b
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2016-10-26 12:20:40 -0700

    complete, can't figure out why there is an extra URL

---
+++ SCRIPTS/textForPortfolio.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var textForPortfolio = [
+  {
+    title:  'Project One',
+    body:   '<p> Cupcake halvah halvah ice cream tart ice cream I love jelly. Danish chocolate bar cupcake toffee pie. Jujubes dragée dessert. Wafer candy canes gingerbread. Chocolate cake bonbon wafer lemon drops gummies liquorice sweet roll. Carrot cake jelly tiramisu chocolate I love. Danish tiramisu cupcake jelly-o dessert cake bonbon. Ice cream croissant jelly beans chocolate cake chupa chups. Donut carrot cake macaroon cupcake sweet roll marshmallow gummi bears I love cookie. I love gummies toffee liquorice. Fruitcake cake liquorice I love chocolate bar powder I love chocolate bar. Tootsie roll candy chupa chups pudding brownie chocolate bar carrot cake. Sugar plum jelly carrot cake chupa chups jelly pudding gingerbread halvah. I love soufflé jelly cake. Apple pie I love I love biscuit bonbon ice cream sesame snaps I love. Bear claw pastry chocolate bar. Powder halvah ice cream carrot cake topping.Cake gummies cake jelly-o sugar plum I love jujubes cupcake. Dessert sesame snaps tootsie roll gingerbread. Chupa chups croissant danish jujubes cupcake. Biscuit marshmallow oat cake. Pudding pastry soufflé I love. Icing bonbon I love pudding sesame snaps soufflé. Macaroon jelly tootsie roll lemon drops dessert gingerbread halvah sugar plum donut. Cookie sweet roll powder halvah. </p>',
+    url:    'https://www.google.com'
+  },
+  {
+    title:    'Project Two',
+    body:     '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Challenge you to a duel lando calrissian nuremberg rally basil fawlty super mario, basil fawlty leader of men lipwig nuremberg rally lando calrissian dolor sit amet worn with distinction will you do the fandango super mario challenge you to a duel bad guy brandy casual style, casual style lipwig super mario 118 118 nuremberg rally east european lando calrissian dolor sit amet leader of men challenge you to a duel old man in pub brandy will you do the fandango graeme souness bad guy worn with distinction basil fawlty nuremberg rally. Inspector clouseau des lynam groomed dis omar sharif, dis old west sheriff groomed tony stark robot moustache omar sharif soup strainer inspector clouseau cesar romero des lynam?</p>',
+    url:      'https://www.yahoo.com'
+  },
+  {
+    title:    'Project Three',
+    body:     '<p>Hodor, hodor - hodor, hodor. Hodor hodor?! Hodor! Hodor hodor, hodor, hodor. Hodor hodor - hodor; hodor hodor. Hodor. Hodor hodor HODOR! Hodor hodor hodor hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor hodor HODOR! Hodor hodor - hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor, hodor. Hodor hodor. Hodor, HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR hodor, hodor hodor hodor hodor! </p> <p>Hodor. Hodor hodor; hodor hodor... Hodor hodor hodor... Hodor hodor hodor? Hodor HODOR hodor, hodor hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor. Hodor hodor. Hodor. Hodor, hodor; hodor hodor, hodor. Hodor hodor? Hodor, hodor. Hodor. Hodor, hodor hodor. Hodor! Hodor hodor, hodor, hodor. Hodor HODOR hodor, hodor hodor... Hodor hodor hodor? Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor hodor... Hodor hodor hodor hodor hodor, hodor, hodor hodor. </p>',
+    url:      'https://en.wikipedia.org/wiki/Game_of_Thrones'
+  }
+];
