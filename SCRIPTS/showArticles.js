d8fdc08a5a227ba9e4be0af116da7ce3156316ac
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2016-10-27 11:52:39 -0700

    tried a few filters but did not work

--- SCRIPTS/showArticles.js
+++ SCRIPTS/showArticles.js
@@ -7,7 +7,8 @@ var articleView = {};
 articleView.handleaboutMe = function () {
   $('.navbar').on('click', '.icon-wondering2',function (){
     var aboutMeTab = $(this).attr('data-content');
-    $('.text-area').filter('.about').hide();
+    // $('.text-area').hide();  //hides everything on page
+  $('data-content').filter('.about').show();
 
 
     console.log('blah');
