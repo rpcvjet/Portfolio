6bb8d3e3f57e7138324f2db1b7a827d6ac8cec63
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2016-10-27 17:37:52 -0700

    adding to master

---
+++ SCRIPTS/showArticles.js
@@ -0,0 +1,20 @@
+'use strict';
+// $(document).ready(function () {
+//   $('Text').hide();
+// });
+var articleView = {};
+
+articleView.handleaboutMe = function () {
+  $('.navbar').on('click', '.icon-wondering2',function (){
+    var aboutMeTab = $(this).attr('data-content');
+    // $('.text-area').hide();  //hides everything on page
+  // $('data-content').filter('.about').show();
+
+
+    console.log('blah');
+
+
+  }); //end of .navbar function
+}; //end of articleView function
+
+articleView.handleaboutMe();
