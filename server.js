3f3231b3e8cb8ffc887d6d33aa7678eb0a485b46
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2016-11-21 14:48:57 -0800

    typos?

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 'use strict';
-
 var express = require('express'),
   requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
-
 var proxyGithub = function(request, response){
   console.log('ROuting Github request for', request.params[0]);
   (requestProxy({
@@ -12,16 +10,12 @@ var proxyGithub = function(request, response){
     headers: {Authorization: 'token ' + process.env.GITHUB_TOKEN}
   }))(request,response);
 };
-
 app.get('/github/*',proxyGithub);
-
 app.use(express.static('./'));
-
 app.get('*', function(request, response) {
   console.log('New request:', request.url);
   response.sendFile('index.html', {root: '.'});
 });
-
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!');
 });
