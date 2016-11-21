663ac43ca3165cd7407ce96150025180eb4b6b30
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2016-11-21 13:31:26 -0800

    added the  commands to follwers and my repositories

---
+++ SCRIPTS/Models/repo.js
@@ -0,0 +1,23 @@
+function(module) {
+  var reposObj = {};
+
+  reposObj.requestRepos = function(callback) {
+    // NOTE: refactor this request into an $.get call
+    $.when(
+      $.get('/github/users/rpcvjet/repositories')
+      $.get('/github/users/rpcvjet/followers')
+
+
+
+    ).done(callback);
+    };
+
+
+      reposObj.withTheAttribute = function(attr) {
+    return reposObj.allRepos.filter(function(aRepo) {
+      return aRepo[attr];
+    });
+  };
+
+  module.reposObj = reposObj;
+})(window);
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 'use strict';
 
 var express = require('express'),
+  requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
+
+var proxyGithub = function(request, response){
+  console.log('ROuting Github request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: {Authorization: 'token ' + process.env.GITHUB_TOKEN}
+  }))(request,response);
+};
+
+app.get('/github/*',proxyGithub );
 
 app.use(express.static('./'));
 
